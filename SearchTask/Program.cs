dac6aa14bad27d55c4051eb9c03a5f3a294b7ec7
ramywael <ramybakry9@gmail.com>
2024-12-04 23:18:24 +0200

    Solve the search

---
+++ SearchTask/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace SearchTask
+{
+    internal class Program
+    {
+        // Write a C# program that reads a list of integers from the user and throws an exception if any numbers are duplicates.
+
+        public static List<int> ReadList()
+        {
+            int value = 0;
+            List<int> numbers = new List<int>();
+            do
+            {
+                Console.WriteLine("Please Enter the numbers of the list");
+                value = Convert.ToInt32(Console.ReadLine());
+                if(numbers.Contains(value) && numbers.Count >= 1)
+                {
+                    throw new InvalidOperationException("The Number is duplicated");
+                }
+                else
+                {
+                    numbers.Add(value);
+                }
+            } while (numbers.Count <5);
+            return numbers;
+        }
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                ReadList();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
---
+++ SearchTask2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace SearchTask2
+{
+    internal class Program
+    {
+
+        public static string ReadString(string massg)
+        {
+            string vowel = "aeiouAEIOU";
+            foreach (char i in massg)
+            {
+                if (vowel.Contains(i))
+                {
+                    return massg;
+                }
+            }
+            throw new Exception("This string doesnot contain vowels");
+        }
+
+        static void Main(string[] args)
+        {
+            string value = Convert.ToString(Console.ReadLine());
+            try
+            {
+                Console.WriteLine(ReadString(value));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
